9836759a3423df6392d3d4b1e670cae1a68a57b6
snehal979 <113917713+snehal979@users.noreply.github.com>
2022-11-16 22:31:04 +0530

    Using interface data

---
+++ InterfacesEmp.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageUC10
+{
+    internal class InterfacesEmp
+    {
+        // interface = comapering the three  method 
+
+        public interface IDataemployeeWage
+        {
+            public void addCompanyEmpWage(string Name, int PerHour, int WorkingDays, int hour);
+            public void DataOfEmployee();
+            public int getTotalEmpWage(int value);
+
+        }
+        public class DataOfEmployee
+        {
+            public string company;
+            public int empRatePerHour;
+            public int numOfWorkingDays;
+            public int maxHoursPerMonth;
+            public int totalEmpWage;
+
+            public DataOfEmployee(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+            {
+                this.company = company;
+                this.empRatePerHour = empRatePerHour;
+                this.numOfWorkingDays = numOfWorkingDays;
+                this.maxHoursPerMonth = maxHoursPerMonth;
+            }
+
+            public void setTotalEmpWage(int totalEmpWage)
+            {
+                this.totalEmpWage = totalEmpWage;
+            }
+
+            public string toString()
+            {
+                return "Total Emp Wage for company: " + this.company + " is : " + this.totalEmpWage;
+            }
+        }
+
+    }
+}
